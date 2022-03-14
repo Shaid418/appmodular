8983852e7a3fe67ec7d218df86efdd48ad17e5ea
Rodolfo Ramirez <rodolram2@gmail.com>
2022-03-14 16:40:08 -0600

    appmodular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'gestion-servicio',loadChildren: () => import('./gestion-servicio/gestion-servicio.module').then(m => m.GestionServicioModule)},
+  { path: 'gestion-financiera',loadChildren: () => import('./gestion-financiera/gestion-financiera.module').then(m => m.GestionFinancieraModule)},
+  { path: 'fabricacion',loadChildren: () => import('./fabricacion/fabricacion.module').then(m=> m.FabricacionModule)},
+  { path: 'recursos-humanos',loadChildren: () => import('./recursos-humanos/recursos-humanos.module').then(m=> m.RecursosHumanosModule)}
+
+  //{ path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
